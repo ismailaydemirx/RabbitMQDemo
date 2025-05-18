0c61e48c0de5b722fe55bcf7054247158ae2bbba
ismailaydemir <67390681+ismailaydemirx@users.noreply.github.com>
2025-05-19 00:58:34 +0300

    initial create
    to use this with docker use this command: "docker run -d --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:management"

---
+++ ConsumerApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+channel.QueueDeclareAsync(
+    queue: "message",
+    durable: true, // to not lose messages if RabbitMQ crashes
+    exclusive: false, // only one consumer can consume the queue
+    autoDelete: false, // the queue will not be deleted when the connection is closed
+    arguments: null // no additional arguments
+    );
+
+Console.WriteLine("Waiting for messages...");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (sender, eventArgs) =>
+{
+    byte[] body = eventArgs.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Recieved: {message}");
+
+    await ((AsyncEventingBasicConsumer)sender).Channel.BasicAckAsync(eventArgs.DeliveryTag, multiple: false);
+};
+
+await channel.BasicConsumeAsync("message", autoAck: false, consumer);
+
+Console.ReadLine();
---
+++ ProducerApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+channel.QueueDeclareAsync(
+    queue: "message",
+    durable: true, // to not lose messages if RabbitMQ crashes
+    exclusive: false, // only one consumer can consume the queue
+    autoDelete: false, // the queue will not be deleted when the connection is closed
+    arguments: null // no additional arguments
+    );
+
+for (int i = 0; i < 10; i++)
+{
+    var message = $"{DateTime.UtcNow} - {Guid.CreateVersion7()}";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync(
+        exchange: string.Empty,
+        routingKey: "message",
+        mandatory: false,
+        basicProperties: new BasicProperties { Persistent = true },
+        body: body
+        );
+
+    Console.WriteLine($" [x] Sent {message}");
+
+    await Task.Delay(2000);
+}
